20c1f6d615c9447d30bb92e6c7befc7b90224e9b
Anton Bobko <antonbobko.by@gmail.com>
2022-10-14 23:35:52 +0300

    3 tests success

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity == "string") {
+    let newSA = Number(sampleActivity);
+    if (newSA) {
+      if (newSA > 0 && newSA < MODERN_ACTIVITY) {
+        return Math.ceil((HALF_LIFE_PERIOD/0.693) * Math.log(MODERN_ACTIVITY/newSA));
+      }
+      return false;
+    }
+    return false;
+  }
+  return false;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let newMem = ''; 
+  let text = '';
+  if (Array.isArray(members)) {
+    for (i = 0; i < members.length; i++) {
+      if (typeof members[i] == 'string') {
+        newMem = members[i].toUpperCase();
+        newMem = newMem.replaceAll(' ', '');
+        text += newMem[0];
+        text = text.split('').sort().join('');
+      }
+    }
+  }
+  return text;
 }
+
+createDreamTeam([
+  '   William Alston ',
+  ' Paul Benacerraf',
+  '  Ross Cameron',
+  '       Gilles Deleuze',
+  '  Arda Denkel ',
+  '  Michael Devitt',
+  '  Kit Fine',
+  ' Nelson Goodman',
+  'David Kolb',
+  '   Saul Kripke',
+  '  Trenton Merricks',
+  '  Jay Rosenberg',
+])
+
 
 module.exports = {
   createDreamTeam
